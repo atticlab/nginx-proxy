593bdbb2cb4076af9f5b2cac2b7b4a9862ad451f
ihor <for0work0@gmail.com>
2017-01-31 16:04:19 +0200

    reset http(s) proxy

--- docker/haproxy/entrypoint.sh
+++ docker/haproxy/entrypoint.sh
@@ -2,4 +2,7 @@
 
 sed -i "s#%HOST%#$HOST#g" /usr/local/etc/haproxy/haproxy.cfg
 
+export http_proxy=''
+export https_proxy=''
+
 CMD ["haproxy", "-f", "/usr/local/etc/haproxy/haproxy.cfg"]
--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -3,4 +3,7 @@
 sed -i "s#%DOMAIN%#$DOMAIN#g" /etc/nginx/conf.d/*
 sed -i "s#%HOST%#$HOST#g" /etc/nginx/conf.d/*
 
+export http_proxy=''
+export https_proxy=''
+
 nginx -g "daemon off;"
